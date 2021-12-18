e8916407e7c6caaa2158802fd631ae434eb65fa7
imdevil <516959708@qq.com>
2021-12-18 14:37:35 +0800

    use lifecycleScope & withContext() to update view

--- app/src/main/java/com/hold/rich/MainActivity.kt
+++ app/src/main/java/com/hold/rich/MainActivity.kt
@@ -2,12 +2,15 @@ package com.hold.rich
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.hold.rich.okex.AccountBalanceResponse
 import com.hold.rich.okex.AccountService
 import com.hold.rich.okex.OkexRetrofit
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 private const val TAG = "Greedy-MainActivity"
 
@@ -18,16 +21,19 @@ class MainActivity : AppCompatActivity() {
 
         val retrofit = OkexRetrofit.retrofit.create(AccountService::class.java)
 
-        GlobalScope.launch {
+        lifecycleScope.launch(Dispatchers.IO) {
             val request = try {
                 retrofit.getBalance("USDT")
             } catch (e: Exception) {
                 Exception("Network request failed")
             }
-            when (request) {
-                is AccountBalanceResponse -> Log.d(TAG, "onCreate: $request")
-                is Exception -> Log.d(TAG, "onCreate: $request")
-                else -> Log.d(TAG, "onCreate: error type")
+            withContext(Dispatchers.Main) {
+                when (request) {
+                    is AccountBalanceResponse -> findViewById<TextView>(R.id.msg).text =
+                        request.toString()
+                    is Exception -> findViewById<TextView>(R.id.msg).text = request.toString()
+                    else -> Log.d(TAG, "onCreate: error type")
+                }
             }
         }
     }
